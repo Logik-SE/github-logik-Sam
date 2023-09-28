4974044eec47e638a238d8ff0f8d4d5fcc2f72cf
klm118 <klm118@users.noreply.github.com>
2023-09-28 16:55:10 +0000

    Blueprint accessPanel

---
+++ accessPanel/rules/kNSmokeproofProduct/kNSmokeproofProduct_action_1/497.js
@@ -0,0 +1,5 @@
+ProductList.parentProduct = "PANEL"+set.kNAccessPanelSet.size.optionValue;
+ProductList.id = "SMOKEPROOF";
+ProductList.type = "Component";
+ProductList.uniqueIdentifier = set.kNAccessPanelSet.size.optionValue + "Smokeproof";
+return ProductList;
